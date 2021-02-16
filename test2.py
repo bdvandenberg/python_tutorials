cbf50ffaa32a55ed3bebeac04b675a7cd3150f7e
Berg B.D. van den (Bas) <Bas.vandenBerg@vanoord.com>
2021-02-16 09:40:57 +0100

    add explanation

---
+++ test2.py
@@ -0,0 +1,13 @@
+"""
+Write a function that has the following input and 
+output:
+
+Input:
+- number
+
+Output:
+- True if the number is even, or
+- False if the number is odd
+
+Test your function before committing to Github.
+"""
